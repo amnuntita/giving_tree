3dbe70f49bdbc05ef125bc9b9dad7995ee85c314
Am <amnuntita19@gmail.com>
2020-12-29 01:36:55 +0700

    install Bootstrap

---
+++ src/component/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from 'reactstrap';
+
+const Main = () => {
+    return <div>
+        Giving Tree Project
+        <div>
+            <Button color="primary">Test</Button>
+        </div>
+    </div>
+}
+
+export default Main;
